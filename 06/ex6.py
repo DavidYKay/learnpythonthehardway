b6e4ce9341e09177c81c6164406b4254cd1b0241
David Young-Chan Kay <davidykay@gmail.com>
2011-03-15 19:30:40 -0400

    Added ex6

---
+++ 06/ex6.py
@@ -0,0 +1,22 @@
+# There are 10 types of people
+x = "There are %d types of people." %10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+# String in string. It gets ''s added because of the %r designation
+print "I said: %r." %x
+print "I also said: '%s'." %y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e 
