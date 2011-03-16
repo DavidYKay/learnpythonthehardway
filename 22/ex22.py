a61691633d2a16854000a56417f3f5fe35c219cf
David Young-Chan Kay <davidykay@gmail.com>
2011-03-16 17:42:40 -0400

    added ex22

---
+++ 22/ex22.py
@@ -0,0 +1,44 @@
+# What have we learned so far
+print
+# Arithmetic
++
+-
+/
+%
+*
+# Logic
+>
+<
+>=
+<=
+False
+# Assignment
+=
+# Strings
+" "
+' '
+""" """
+''' '''
+%r - raw
+%
++ - concat w/o space
+, - concat w/ space
+\n - newline
+\t - tab
+# Lists
+( )
+
+# Keywords
+def
+from
+import
+# Functions
+raw_input()
+# File handling
+open
+read
+write
+close
+truncate
+# Modules
+argv
