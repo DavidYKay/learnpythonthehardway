aac033a88bb28c3e9990fba586e39f38199b7522
David Young-Chan Kay <davidykay@gmail.com>
2011-03-16 14:51:18 -0400

    Added 17.

---
+++ 17/ex17.py
@@ -0,0 +1,26 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+#how do we do these on one line?
+input = open(from_file)
+indata = input.read()
+
+#indata = open(from_file).read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+output = open(to_file, 'w')
+output.write(indata)
+
+print "Alright, all done."
+
+output.close()
+input.close()
