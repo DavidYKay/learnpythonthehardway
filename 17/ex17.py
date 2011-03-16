8c957b68bd61b699ec2adecb445898dcae8e5397
David Young-Chan Kay <davidykay@gmail.com>
2011-03-16 14:55:01 -0400

    Shortened ex17

--- 17/ex17.py
+++ 17/ex17.py
@@ -1,26 +1,30 @@
 from sys import argv
 from os.path import exists
 
-script, from_file, to_file = argv
+open(argv[2], 'w').write(open(argv[1]).read()) 
 
-print "Copying from %s to %s" % (from_file, to_file)
+#script, from_file, to_file = argv
+#indata = open(from_file).read()
+#open(to_file, 'w').write(indata)
+
+#print "Copying from %s to %s" % (from_file, to_file)
 
 #how do we do these on one line?
-input = open(from_file)
-indata = input.read()
+#input = open(from_file)
+#indata = input.read()
 
-#indata = open(from_file).read()
 
-print "The input file is %d bytes long" % len(indata)
+#print "The input file is %d bytes long" % len(indata)
 
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
+#print "Does the output file exist? %r" % exists(to_file)
+#print "Ready, hit RETURN to continue, CTRL-C to abort."
+#raw_input()
 
-output = open(to_file, 'w')
-output.write(indata)
+#output = open(to_file, 'w')
+#output.write(indata)
 
-print "Alright, all done."
 
-output.close()
-input.close()
+#print "Alright, all done."
+
+#output.close()
+#input.close()
