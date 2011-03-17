c19e212319fb05a19a0737d4f12fa5191f9a8045
David Young-Chan Kay <davidykay@gmail.com>
2011-03-17 12:40:29 -0400

    Now passing all tests.

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -39,12 +39,33 @@ categories = {
         ]),
 }
 
+def convert_word(word):
+    # Check each category
+    for category_name in categories.keys():
+        if word in categories[category_name]:
+            return((category_name, word))
+    return None
+
+def convert_number(string):
+    try:
+        return int(string)
+    except ValueError:
+        return None
+
 def scan(sentence):
     results = []
     words = sentence.split()
     for word in words:
-        # Check each category
-        for category_name in categories.keys():
-            if word in categories[category_name]:
-                results.append((category_name, word))
+        # Is this a word?
+        result = convert_word(word)
+        if result != None:
+            results.append(result)
+            continue
+        # Is this a number?
+        result = convert_number(word)
+        if result != None:
+            results.append(('number', result))
+            continue
+        # Error.
+        results.append(('error', word))
     return results
--- projects/ex48/tests/lexicon_tests.py
+++ projects/ex48/tests/lexicon_tests.py
@@ -33,10 +33,10 @@ def test_nouns():
                           ('noun', 'princess')])
 
 def test_numbers():
-    assert_equal(lexicon.scan("1234"), [('number', '1234')])
+    assert_equal(lexicon.scan("1234"), [('number', 1234)])
     result = lexicon.scan("3 91234")
-    assert_equal(result, [('number', '3'),
-                          ('number', '91234')])
+    assert_equal(result, [('number', 3),
+                          ('number', 91234)])
 
 def test_errors():
     assert_equal(lexicon.scan("ASDFADFASDF"), [('error', 'ASDFADFASDF')])
