2e2e8400a3fb7731118eb98bd9e505de70677328
David Young-Chan Kay <davidykay@gmail.com>
2011-03-17 12:29:21 -0400

    lexicon: now supports multiple word types.

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -7,10 +7,25 @@ directions = set([
 'down',
 ])
 
+verbs = set([
+'go',
+'kill',
+'eat',
+])
+
+categories = {
+    'direction': directions,
+    'verb': verbs,
+}
+
 def scan(sentence):
     results = []
     words = sentence.split()
     for word in words:
-        if word in directions:
-            results.append(('direction', word))
+        # Check each category
+        #for category_name, category_words in categories:
+        for category_name in categories.keys():
+            category_words = categories[category_name]
+            if word in category_words:
+                results.append((category_name, word))
     return results
