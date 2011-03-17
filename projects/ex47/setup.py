a7cbb30a0bbdab05ef57b9735a7171705c4fed33
David Young-Chan Kay <davidykay@gmail.com>
2011-03-17 11:48:03 -0400

    Added ex47

---
+++ projects/ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from disutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'David Kay',
+    'url': 'http://project.com',
+    'download_url': 'http://project.com/download',
+    'author_email': 'davidykay@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'],
+    'scripts': [],
+    'name': 'projectname',
+}
+
+setup(**config)
